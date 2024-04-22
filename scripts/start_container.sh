fca5ee9460a0da9421ded8cef4b9b693eb298970
Komal G <54700625+ghulekomal@users.noreply.github.com>
2024-04-22 10:34:05 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,6 +6,7 @@ echo "Pull the Docker image from Docker Hub"
 docker pull gkomal1616/simple-python-flask-app:v1
 
 #comment
+#echo
 echo "comment"
 # Run the Docker image as a container
 echo "Run the Docker image as a container"
