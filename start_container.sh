515c613974df5f60052abcb99cb3886f3198972f
Komal G <54700625+ghulekomal@users.noreply.github.com>
2024-04-22 02:44:31 +0000

    codedeploy files

---
+++ start_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+echo "Pull the Docker image from Docker Hub"
+docker pull gkomal1616/simple-python-flask-app:v1
+
+# Run the Docker image as a container
+echo "Run the Docker image as a container"
+docker run -d -p 5000:5000 gkomal1616/simple-python-flask-app:v1
---
+++ stop_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Stop the running container (if any)
+echo "Stop the running container (if any)"
+
+containerid=`docker ps | awk -F " " '{print $1}'`
+docker rm -f $containerid
